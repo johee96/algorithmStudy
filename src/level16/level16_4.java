3fe49c2e853a4e125e160375318dfc9407d7aaa6
user <user@DESKTOP-R88BQ0K>
2019-08-26 21:56:57 +0900

    level16

---
+++ src/level16/level16_4.java
@@ -0,0 +1,36 @@
+package level16;
+import java.util.Scanner;
+
+public class level16_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		long max = 0;
+		int n = sc.nextInt();
+		long data0[] = new long[n];
+		long data1[] = new long[n];
+		long data2[] = new long[n];
+
+		for (int i = 0; i < n; i++) {
+			int v = sc.nextInt();
+			if (i == 0) {
+				data0[i] = 0;
+				data1[i] = v;
+				data2[i] = 0;
+			} else {
+				long m = Math.max(data0[i - 1], data1[i - 1]);
+				m = Math.max(m, data2[i - 1]);
+				data0[i] =m;
+				data1[i] = v + data0[i - 1];
+				data2[i] = v + data1[i - 1];
+			}
+		}
+		max = Math.max(data0[n - 1], data1[n - 1]);
+		max = Math.max(max, data2[n - 1]);
+		System.out.println(max);
+	}
+
+
+}
---
+++ src/level16/level16_5.java
@@ -0,0 +1,35 @@
+package level16;
+import java.util.Scanner;
+
+public class level16_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long btnr[] = new long[n];
+		long btng[] = new long[n];
+		long btnb[] = new long[n];
+		
+		for(int i=0;i<n;i++) {
+			int r = sc.nextInt();
+			int g = sc.nextInt();
+			int b = sc.nextInt();
+			if(i==0) {
+				btnr[i] = r;
+				btng[i] = g;
+				btnb[i] = b;
+			}else {
+				btnr[i] = r +Math.max(btng[i-1], btnb[i-1]);
+				btng[i] = g +Math.max(btnr[i-1], btnb[i-1]);
+				btnb[i] = b +Math.max(btng[i-1], btnr[i-1]);				
+			}
+						
+		}
+		long max = Math.max(btnr[n-1], btng[n-1]);
+		max = Math.max(btnb[n-1], max);
+		System.out.println(max);
+		
+	}
+}
