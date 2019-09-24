c5745dcc41f7f41edb96ac21071d43b65a1b2f70
johee96 <whgml1@gmail.com>
2019-09-24 09:09:56 +0900

    level 17

--- src/level17/level17_1.java
+++ src/level17/level17_1.java
@@ -1,10 +1,32 @@
 package level17;
 
+import java.util.Scanner;
+
 public class level17_1 {
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int map[][] = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				int v = sc.nextInt();
+				if(i==0 && j==0)
+					map[0][0] =v;
+				else if(i==0){
+					map[0][j] = map[0][j-1] + v;
+				}else if(j==0) {
+					map[i][0] = map[i-1][0] + v;
+				}else {
+					map[i][j] = Math.max(map[i][j-1], map[i-1][j])+v;					
+				}
+			}
+		}
+		System.out.println(map[n-1][m-1]);
 	}
 
 }
--- src/level17/level17_2.java
+++ src/level17/level17_2.java
@@ -1,10 +1,26 @@
 package level17;
-
+import java.util.Scanner;
 public class level17_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int data[] = new int[n];
+		int max = -1;
+		for (int i = 0; i < n; i++) {
+			int v = sc.nextInt();
+			if (i == 0)
+				data[0] = v;
+			else
+				data[i] = Math.max(v, data[i - 1] + v);
+
+			if (max < data[i])
+				max = data[i];
+
+		}
+		System.out.print(max);
 	}
 
 }
