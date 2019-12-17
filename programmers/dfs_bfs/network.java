5e42d1fc08f36996f2686e9dd15df909d90f4ceb
johee96 <whgml1@gmail.com>
2019-12-17 16:14:38 +0900

    programmers_bfs/dfs

---
+++ programmers/dfs_bfs/network.java
@@ -0,0 +1,40 @@
+package dfs_bfs;
+
+public class network {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		networkSolution sol = new networkSolution();
+		int numbers[][] = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
+		int re = sol.solution(3, numbers);
+		System.out.println(re);
+	}
+
+}
+
+class networkSolution {
+	static boolean visited[];
+
+	public int solution(int n, int[][] computers) {
+		int answer = 0;
+		visited = new boolean[n];
+
+		for (int i = 0; i < n; i++) {
+			if (!visited[i]) {
+				answer++;
+				dfs(i, computers,n);
+			}
+		}
+		return answer;
+	}
+
+	private void dfs(int v, int[][] computers,int n) {
+		if(v >= n || visited[v])
+			return;
+		visited[v] = true;
+		for(int i=0;i<n;i++) 
+			if(computers[v][i] == 1)
+				dfs(i,computers,n);
+		
+	}
+}
---
+++ programmers/dfs_bfs/targetNumber.java
@@ -0,0 +1,37 @@
+package dfs_bfs;
+
+public class targetNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		targetNumberSolution sol = new targetNumberSolution();
+		int numbers[] = {1,1,1,1,1};
+		int re = sol.solution(numbers,3);
+		System.out.println(re);
+	}
+
+}
+class targetNumberSolution {
+	static int count = 0;
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        dfs(numbers,target,0,0);
+        answer = count;
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int target,int idx, int current) {
+    	
+    	if(idx == numbers.length) {
+    		if(current == target)
+    			count++;
+    		return;
+    	}
+    	int v = numbers[idx];
+    	dfs(numbers,target,idx+1,current + v);
+    	dfs(numbers,target,idx+1,current + v*-1);    	
+    	
+    }
+
+}
+
