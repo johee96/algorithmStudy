401c2d2c576a537f1653bb3f3c77e59ab590c856
johee96 <whgml1@gmail.com>
2020-01-09 15:25:35 +0900

    2020.01.09

--- src/swexpert/D5/수정이의타일자르기_SW1812.java
+++ src/swexpert/D5/수정이의타일자르기_SW1812.java
@@ -1,7 +1,6 @@
 package swexpert.D5;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class 수정이의타일자르기_SW1812 {
     public static void main(String[] args) {
@@ -10,65 +9,58 @@ public class 수정이의타일자르기_SW1812 {
         for (int t = 1; t <= tc; t++) {
             int n = sc.nextInt();
             int m = sc.nextInt();
-            ArrayList<Integer> dataN = new ArrayList<>();
+            ArrayList<Integer> tiles = new ArrayList<>();           //입력받은 n개의 타일들
             for (int i = 0; i < n; i++) {
                 int v = sc.nextInt();
-                if (!dataN.contains(v))
-                    dataN.add(v);
-                else {
-                    dataN.remove(Integer.valueOf(v));
-                    dataN.add(v + 1);
+                tiles.add((int) Math.pow(2, v));
+            }
+            tiles.sort(Comparator.reverseOrder());
+
+            System.out.println("#" + t + " " + getTiles(n, m, tiles));
+
+        }
+    }
+
+    private static int getTiles(int n, int m, ArrayList<Integer> tiles) {
+        int result = 1;
+        ArrayList<Rect> rects = new ArrayList<>();
+        rects.add(new Rect(m, m));
+
+        for (int i = 0; i < n; i++) {
+            int targetTile = tiles.get(i);
+            boolean addTile = true;
+            int size = rects.size();
+            for (int j = 0; j < size; j++) {
+                Rect tmp = rects.get(j);
+                if (tmp.w - targetTile >= 0 && tmp.h - targetTile >= 0) {
+                    //타일 사용할 수 있음
+                    addTile = false;
+                    if (tmp.h - targetTile > 0 && targetTile > 0)
+                        rects.add(new Rect(targetTile, tmp.h - targetTile));
+                    if (tmp.h > 0 && tmp.w - targetTile > 0)
+                        rects.add(new Rect(tmp.w - targetTile, tmp.h));
+                    rects.remove(j);
+                    break;
                 }
             }
-            int nSum = dataNSum(dataN);
-            int mSum = dataMSum(m);
-            int result = check(nSum, mSum);
+            if (addTile) {
+                result++;
+                rects.add(new Rect(targetTile, m - targetTile));
+                rects.add(new Rect(m - targetTile, m));
 
-            System.out.println("#"+t+" "+result);
-
-
-        }
-    }
-
-    private static int dataNSum(ArrayList<Integer> dataN) {
-        int dataNSum = 0;
-        for (int i = 0; i < dataN.size(); i++)
-            dataNSum += dataN.get(i);
-        System.out.println("dataNsum: "+dataNSum);
-        return dataNSum;
-    }
-
-    private static int dataMSum(int m) {
-        int sum = 0;
-        while (m > 1) {
-
-            int index = 1;
-            int tmp = m;
-            while (true) {
-                if (tmp < Math.pow(2, index))
-                    break;
-                index++;
             }
 
-            sum += index-1;
-            m -= Math.pow(2, index);
-
         }
-        System.out.println("dataMSum: "+sum);
-
-        return sum;
-    }
-
-    private static int check(int nSum, int mSum) {
-        int result = 1;
-        while (true) {
-            if (nSum <= mSum * result)
-                break;
-            result++;
-        }
-
-        System.out.println("result: "+result);
-
         return result;
     }
+
+    static class Rect {
+        int w, h;
+        public Rect(int w, int h) {
+            this.w = w;
+            this.h = h;
+        }
+
+    }
+
 }
