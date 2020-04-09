37f96a626879476660cb23196d051102b58327c1
johee96 <whgml1@gmail.com>
2020-04-09 20:55:57 +0900

    programmers : dfs_bfs 단어변환

--- src/programmers/dfs_bfs/wordChange.java
+++ src/programmers/dfs_bfs/wordChange.java
@@ -7,7 +7,6 @@ public class wordChange {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		wordChangeSolution sol = new wordChangeSolution();
-		int numbers[][] = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
 		String words[] = { "hot", "dot", "dog", "lot", "log", "cog" };
 		int re = sol.solution("hit", "cog", words);
 		System.out.println(re);
@@ -24,35 +23,32 @@ class wordChangeSolution {
 		visited = new boolean[words.length];
 
 		// target이 words안에 없을 경우 0 return
-		if (!Arrays.asList(words).contains(target))
+		if (!Arrays.asList(words).contains(target) || begin.equals(target))
 			return 0;
-		
+
 		dfs(begin, target, words, 0);
 		answer = result;
 		return answer;
 	}
 
 	private void dfs(String begin, String target, String[] words, int count) {
+		System.out.println("begin: " + begin + " / target: " + target + " /cnt : " + count);
+
 		if (begin.equals(target)) {
 			if (result > count)
 				result = count;
+			System.out.println("*" + result);
 			return;
 		}
-
 		for (int i = 0; i < words.length; i++) {
-			if(visited[i])
-				continue;
 			int cnt = 0;
 			for (int j = 0; j < begin.length(); j++)
 				if (begin.charAt(j) != words[i].charAt(j))
 					cnt++;
 			if (cnt == 1 && !visited[i]) {
 				visited[i] = true;
-				System.out.println("[1] words[i] : "+words[i] +" begin: "+begin);
 				dfs(words[i], target, words, count + 1);
-				
-				System.out.println("[2] words[i] : "+words[i] +" begin: "+begin);
-				dfs(begin, target, words, count);
+				visited[i] = false;
 
 			}
 
