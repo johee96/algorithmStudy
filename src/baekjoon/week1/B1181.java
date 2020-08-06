3df95693a9dda22c94e03e47faed44a3af2eaab4
johee96 <whgml1@gmail.com>
2020-08-06 22:26:00 +0900

    Baekjoon - week1 : contains 제외 - week2 : B10828

--- src/baekjoon/week1/B1181.java
+++ src/baekjoon/week1/B1181.java
@@ -1,11 +1,35 @@
 package baekjoon.week1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class B1181 {
     public static void main(String[] args) {
+        usingSet();
+    }
+
+    private static void usingSet() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Set<String> setData = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            String value = sc.next();
+            setData.add(value);
+        }
+        ArrayList<String> data = new ArrayList<>(setData);
+
+        Collections.sort(data, (o1, o2) -> {
+            Integer v1 = o1.length();
+            Integer v2 = o2.length();
+            if (v1 == v2)
+                return o1.compareTo(o2);
+            return v1 - v2;
+        });
+        for (int i = 0; i < data.size(); i++) {
+            System.out.println(data.get(i));
+        }
+    }
+
+    private void usingContains() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         ArrayList<String> data = new ArrayList<>();
@@ -24,7 +48,6 @@ public class B1181 {
         for (int i = 0; i < data.size(); i++) {
             System.out.println(data.get(i));
         }
-
     }
 }
 
---
+++ src/baekjoon/week2/B10828.java
@@ -0,0 +1,112 @@
+package baekjoon.week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class B10828 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        MyStack numStack = new MyStack();
+        for (int i = 0; i < n; i++) {
+            String data = br.readLine();
+            String[] splitData = data.split("\\s");
+            if (splitData.length == 2) {
+                numStack.push(Integer.valueOf(splitData[1]));
+                continue;
+            }
+            switch (splitData[0]) {
+                case "pop":
+                    System.out.println(numStack.pop());
+                    break;
+                case "top":
+                    System.out.println(numStack.peek());
+                    break;
+                case "size":
+                    System.out.println(numStack.size());
+                    break;
+                case "empty":
+                    System.out.println(numStack.isEmpty());
+
+                    break;
+            }
+        }
+    }
+
+    private static void usingStack() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        Stack<Integer> numStack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            String data = br.readLine();
+            String[] splitData = data.split("\\s");
+            if (splitData.length == 2) {
+                numStack.push(Integer.valueOf(splitData[1]));
+                continue;
+            }
+            switch (splitData[0]) {
+                case "pop":
+                    if (numStack.size() == 0) {
+                        System.out.println(-1);
+                        break;
+                    }
+                    System.out.println(numStack.pop());
+                    break;
+                case "top":
+                    if (numStack.size() == 0) {
+                        System.out.println(-1);
+                        break;
+                    }
+                    System.out.println(numStack.peek());
+                    break;
+                case "size":
+                    System.out.println(numStack.size());
+                    break;
+                case "empty":
+                    if (numStack.isEmpty())
+                        System.out.println(1);
+                    else
+                        System.out.println(0);
+                    break;
+            }
+        }
+    }
+
+    private static class MyStack {
+        ArrayList<Object> data;
+
+        MyStack() {
+            data = new ArrayList<Object>();
+        }
+
+        public Object pop() {
+            if (data.size() == 0) return -1;
+            int item = (int) data.get(data.size() - 1);
+            data.remove(data.size() - 1);
+            return item;
+        }
+
+        public void push(int item) {
+            data.add(item);
+        }
+
+        public Object peek() {
+            if (data.size() == 0) return -1;
+            return (int) data.get(data.size() - 1);
+        }
+
+        public int isEmpty() {
+            return data.isEmpty() ? 1 : 0;
+        }
+
+        public int size() {
+            return data.size();
+        }
+    }
+}
