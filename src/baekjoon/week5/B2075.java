464d9fefce062c4c41d9328ca38bb24106bead7a
johee96 <whgml1@gmail.com>
2020-08-29 16:52:31 +0900

    Baekjoon - week5 B2075

---
+++ src/baekjoon/week5/B2075.java
@@ -0,0 +1,27 @@
+package baekjoon.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Long> data = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                Long val = Long.parseLong(st.nextToken());
+                data.add(val);
+            }
+        }
+        Collections.sort(data, Collections.reverseOrder());
+        System.out.print(data.get(n - 1));
+
+    }
+}
