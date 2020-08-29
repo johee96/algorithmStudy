94f1cf420e0f1d1899dbf4d1dfa1488f2cdfd2de
johee96 <whgml1@gmail.com>
2020-08-29 16:44:51 +0900

    Baekjoon - B5648

---
+++ src/baekjoon/week5/B5648.java
@@ -0,0 +1,42 @@
+package baekjoon.week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class B5648 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        ArrayList<Long> data = new ArrayList<>();
+        while (true) {
+            while (st.hasMoreTokens()) {
+                Long value = reverseValue(st.nextToken());
+                data.add(value);
+                n--;
+            }
+            if (n == 0)
+                break;
+            st = new StringTokenizer(br.readLine());
+        }
+
+        Collections.sort(data);
+        for (int i = 0; i < data.size(); i++)
+            System.out.println(data.get(i));
+
+    }
+
+    private static Long reverseValue(String value) {
+        StringBuilder reStr = new StringBuilder();
+        for (int i = value.length() - 1; i >= 0; i--) {
+            reStr.append(value.charAt(i));
+        }
+        Long result = Long.parseLong(reStr.toString());
+
+        return result;
+    }
+}
